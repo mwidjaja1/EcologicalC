e9d28447a52f5972c3063ed1a7e545af561b9899
mwidjaja1 <Matthew@Matthews-MacBook-Pro.local>
2014-06-20 20:53:43 -0400

    Version 2 - LV Modifications
    Began testing the script for the needed modifications to sustain the LV
    Models. This means testing more complex functions & removing any excess
    data.

--- rk4gsl.c
+++ rk4gsl.c
@@ -13,22 +13,16 @@
 
 int func (double t, const double y[], double f[], void *params)
 {
-	double mu = *(double *)params;
-	f[0] = y[1];
-	f[1] = -y[0] - mu*y[1]*(y[0]*y[0] - 1);
+	f[0] = y[0] * (1 - y[0] - y[1]);
+	f[1] = y[1] * (1 - y[1] + y[0]);
 	return GSL_SUCCESS;
 }
 
 int jac (double t, const double y[], double *dfdy, double dfdt[], void *params)
 {
-	double mu = *(double *)params;
 	gsl_matrix_view dfdy_mat
-	= gsl_matrix_view_array (dfdy, 2, 2);
+	= gsl_matrix_view_array (dfdy, 0, 0);
 	gsl_matrix * m = &dfdy_mat.matrix;
-	gsl_matrix_set (m, 0, 0, 0.0);
-	gsl_matrix_set (m, 0, 1, 1.0);
-	gsl_matrix_set (m, 1, 0, -2.0*mu*y[0]*y[1] - 1.0);
-	gsl_matrix_set (m, 1, 1, -mu*(y[0]*y[0] - 1.0));
 	dfdt[0] = 0.0;
 	dfdt[1] = 0.0;
 	return GSL_SUCCESS;
@@ -36,15 +30,14 @@ int jac (double t, const double y[], double *dfdy, double dfdt[], void *params)
 
 int main (void)
 {
-	double mu = 10;
-	gsl_odeiv2_system sys = {func, jac, 2, &mu};
+	gsl_odeiv2_system sys = {func, jac, 2};
 	
 	gsl_odeiv2_driver * d =
 	gsl_odeiv2_driver_alloc_y_new (&sys, gsl_odeiv2_step_rk8pd,
 											 1e-6, 1e-6, 0.0);
 	int i;
-	double t = 0.0, t1 = 100.0;
-	double y[2] = { 1.0, 0.0 };
+	double t = 0.0, t1 = 20.0;
+	double y[2] = { 3.0, 1.0 };
 	
 	for (i = 1; i <= 100; i++)
 	{
@@ -57,7 +50,7 @@ int main (void)
 			break;
 		}
 		
-      printf ("%.5e %.5e %.5e\n", t, y[0], y[1]);
+      printf ("%f\t %f\t %f\n", t, y[0], y[1]);
 	}
 	
 	gsl_odeiv2_driver_free (d);
